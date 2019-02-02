435ddf00011b87c91f58f1e59992056e59595efd
Christopher Bishop <domingobishop@gmail.com>
2019-02-02 18:31:34 +0000

    gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,7 +9,7 @@ module.exports = function (grunt) {
             },
             dist: {
                 files: {
-                    'css/base-sass.css': 'css/sass/base-sass.scss'
+                    'css/gw-styles.css': 'sass/gw-sass.scss'
                 }
             }
         },
@@ -19,35 +19,7 @@ module.exports = function (grunt) {
             },
             target: {
                 files: {
-                    'css/base-sass.min.css': ['css/base-sass.css']
-                }
-            }
-        },
-        watch: {
-            scripts: {
-                files: 'js/*.js',
-                tasks: ['qunit', 'concat', 'uglify']
-            },
-            css: {
-                files: 'css/sass/*.scss',
-                tasks: ['sass', 'cssmin']
-            }
-        },
-        qunit: {
-            small: {
-                src: ['js/tests/**/*.html'],
-                options: {
-                    page: {
-                        viewportSize: {width: 300, height: 400}
-                    }
-                }
-            },
-            large: {
-                src: ['js/tests/**/*.html'],
-                options: {
-                    page: {
-                        viewportSize: {width: 1024, height: 400}
-                    }
+                    'css/gw-styles.min.css': ['css/gw-styles.css']
                 }
             }
         },
@@ -56,8 +28,8 @@ module.exports = function (grunt) {
                 separator: ';'
             },
             dist: {
-                src: ['js/generic-utilities.js', 'js/mega-menu.js', 'js/mitigate-target-blank.js', 'js/back-to-top.js', 'js/cookie-notice.js', 'js/global-search.js', 'js/image-caption.js', 'js/tna-global.js','js/run-on-page-load.js'],
-                dest: 'js/compiled/tna-base.js'
+                src: ['js/gw.js'],
+                dest: 'js/compiled/gw.js'
             }
         },
         uglify: {
@@ -66,20 +38,18 @@ module.exports = function (grunt) {
             },
             my_target: {
                 files: {
-                    'js/compiled/tna-base.min.js': ['js/compiled/tna-base.js']
+                    'js/compiled/gw.min.js': ['js/compiled/gw.js']
                 }
             }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-sass');
-    grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-contrib-qunit');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
 
     // Default task(s).
-    grunt.registerTask('default', ['sass', 'cssmin', 'concat', 'uglify', 'qunit', 'watch']);
+    grunt.registerTask('default', ['sass', 'cssmin', 'concat', 'uglify']);
 
 };
