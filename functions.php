cf270d09d0379972fa64f0136203eb7bbfe78a05
Christopher Bishop <domingobishop@gmail.com>
2019-02-17 13:59:10 +1030

    new buttons and vineyard css

--- functions.php
+++ functions.php
@@ -23,3 +23,4 @@ add_filter( 'next_posts_link_attributes', 'posts_link_attributes' );
 add_filter( 'previous_posts_link_attributes', 'posts_link_attributes' );
 
 add_shortcode( 'wine', 'wine_shortcode' );
+add_shortcode( 'vineyard', 'vineyard_shortcode' );
--- inc/functions-wines.php
+++ inc/functions-wines.php
@@ -35,3 +35,37 @@ function wine_html( $img, $title, $link ) {
 
     return sprintf( $html, $link, $img, $title, $title );
 }
+
+function vineyard_shortcode( $atts )
+{
+    $a = shortcode_atts(array(
+        'image' => '',
+        'title' => '',
+        'link' => '',
+        'text' => ''
+    ), $atts);
+
+    $first = '<div class="vineyard">';
+    $last = '</div>';
+
+    $html = $first . vineyard_html( $a['image'], $a['title'], $a['link'], $a['text'] ) . $last;
+
+    return $html;
+}
+
+function vineyard_html( $img, $title, $link, $text ) {
+
+    $html = '<div class="col-md-4">';
+    $html .= '<img src="%s" alt="%s" class="img-responsive">';
+    $html .= '</div>';
+    $html .= '<div class="col-md-8">';
+    $html .= '<h2>%s</h2>';
+    $html .= '<p>%s';
+    if ($link) {
+        $html .= '<a href="%s" class="btn">Find out more</a>';
+    }
+    $html .= '</p>';
+    $html .= '</div>';
+
+    return sprintf( $html, $img, $title, $title, $text, $link );
+}
