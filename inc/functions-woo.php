9c66c2adc4b5f6c4108a72732df51fd48df4131f
Christopher Bishop <domingobishop@gmail.com>
2019-03-09 11:23:13 +0000

    Hide shipping rates when free shipping is available

--- inc/functions-woo.php
+++ inc/functions-woo.php
@@ -143,3 +143,22 @@ function woo_remove_product_tabs( $tabs ) {
     unset( $tabs['additional_information'] );   // Remove the additional information tab
     return $tabs;
 }
+
+/**
+ * Hide shipping rates when free shipping is available.
+ * Updated to support WooCommerce 2.6 Shipping Zones.
+ *
+ * @param array $rates Array of rates found for the package.
+ * @return array
+ */
+function my_hide_shipping_when_free_is_available( $rates ) {
+    $free = array();
+    foreach ( $rates as $rate_id => $rate ) {
+        if ( 'free_shipping' === $rate->method_id ) {
+            $free[ $rate_id ] = $rate;
+            break;
+        }
+    }
+    return ! empty( $free ) ? $free : $rates;
+}
+add_filter( 'woocommerce_package_rates', 'my_hide_shipping_when_free_is_available', 100 );
