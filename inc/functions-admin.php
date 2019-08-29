57913b2e3f97861d61b2745ab82feb2d9a495924
domingobishop <domingobishop@gmail.com>
2019-08-29 14:42:44 +0100

    submenu and pages

--- inc/functions-admin.php
+++ inc/functions-admin.php
@@ -5,6 +5,8 @@
 
 function grosset_settings_menu() {
     add_menu_page( 'Grosset Wines settings', 'Grosset', 'administrator', 'grosset-settings-page', 'grosset_settings_page', 'dashicons-admin-generic', 21  );
+    add_submenu_page( 'grosset-settings-page', 'Homepage settings', 'Home', 'administrator', 'grosset-settings-page/home', 'grosset_home_settings_page');
+    add_submenu_page( 'grosset-settings-page', 'Wines landing page settings', 'Wines', 'administrator', 'grosset-settings-page/wines', 'grosset_wines_settings_page');
     add_action( 'admin_init', 'grosset_settings_page_admin' );
 }
 
@@ -28,6 +30,36 @@ function grosset_settings_page_admin() {
     }
 }
 
+function grosset_wines_settings_page() {
+    if (!current_user_can('administrator'))  {
+        wp_die( __('You do not have sufficient pilchards to access this page.')    );
+    }
+    ?>
+    <style>
+        .g-admin input[type=text] {
+            width: 100%;
+            max-width: 480px;
+        }
+        .g-admin textarea {
+            width: 100%;
+            max-width: 480px;
+            height: 12em;
+        }
+    </style>
+    <div class="wrap g-admin">
+        <h1>Wines landing page settings</h1>
+        <form method="post" action="options.php" novalidate="novalidate">
+            <?php settings_fields( 'gw-group' ); ?>
+            <?php do_settings_sections( 'gw-group' ); ?>
+
+            <h2>Content</h2>
+
+            <?php submit_button(); ?>
+        </form>
+    </div>
+    <?php
+}
+
 function grosset_settings_page() {
     if (!current_user_can('administrator'))  {
         wp_die( __('You do not have sufficient pilchards to access this page.')    );
@@ -46,6 +78,36 @@ function grosset_settings_page() {
     </style>
     <div class="wrap g-admin">
         <h1>Grosset Wines theme settings</h1>
+        <form method="post" action="options.php" novalidate="novalidate">
+            <?php settings_fields( 'gw-group' ); ?>
+            <?php do_settings_sections( 'gw-group' ); ?>
+
+            <h2>General settings</h2>
+
+            <?php submit_button(); ?>
+        </form>
+    </div>
+    <?php
+}
+
+function grosset_home_settings_page() {
+    if (!current_user_can('administrator'))  {
+        wp_die( __('You do not have sufficient pilchards to access this page.')    );
+    }
+    ?>
+    <style>
+        .g-admin input[type=text] {
+            width: 100%;
+            max-width: 480px;
+        }
+        .g-admin textarea {
+            width: 100%;
+            max-width: 480px;
+            height: 12em;
+        }
+    </style>
+    <div class="wrap g-admin">
+        <h1>Grosset Wines homepage settings</h1>
         <form method="post" action="options.php" novalidate="novalidate">
             <?php settings_fields( 'gw-group' ); ?>
             <?php do_settings_sections( 'gw-group' ); ?>
