ddf1d82299ca8787f39a8051ebfe54dc0b675763
domingobishop <domingobishop@gmail.com>
2021-07-23 11:49:48 +0100

    new woo template and css

---
+++ page-containerwidth.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Template Name: Container width
+ *
+ */
+get_header(); ?>
+
+<?php while (have_posts()) : the_post(); ?>
+
+    <main id="main" class="main" role="main">
+        <div class="content">
+            <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="entry-header">
+                                <h1>
+                                    <?php the_title(); ?>
+                                </h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="entry-content clearfix">
+                                <?php the_content(); ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </article>
+        </div>
+    </main>
+
+<?php endwhile; ?>
+
+<?php get_footer(); ?>
